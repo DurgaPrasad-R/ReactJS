4dd50c33b9e709f2e91404c16171e525273ee230
Runkana Durga Prasad <runkanaprasad1@gmail.com>
2023-10-21 18:41:44 +0530

    updates

---
+++ Tasks/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+const heading = React.createElement("h1", { id: "first" }, "Hello World");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
