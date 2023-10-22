4254c905eb640726acd72cd325ce1b1604302b5c
Runkana Durga Prasad <runkanaprasad1@gmail.com>
2023-10-22 20:51:16 +0530

    updates

--- Tasks/app.js
+++ Tasks/app.js
@@ -1,5 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-const heading = React.createElement("h1", { id: "first" }, "Hello World");
+import React from "react";
+import mainLogo from "./images/logo.png";
+import { CgProfile } from "react-icons/cg";
+import ReactDOM from "react-dom/client";
+const logo = (
+  <div className="logo">
+    <img src={mainLogo}></img>
+  </div>
+);
+const Dive = () => {
+  return (
+    <div id="title1" className="header">
+      {logo}
+      <div className="inp">
+        <input type="text" />
+      </div>
+      <div className="person">
+        <CgProfile />
+      </div>
+    </div>
+  );
+};
+const title = React.createElement("div", { id: "title" }, [
+  React.createElement("h1", {}, "Head1"),
+  React.createElement("h2", {}, "Head2"),
+  React.createElement("h3", {}, "Head3"),
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+const main = ReactDOM.createRoot(document.getElementById("main"));
+main.render(title);
+root.render(<Dive />);
